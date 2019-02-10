526a234e845b3a39a2e91e61a6e56b5f25a5d809
mochi <n1046746@ntub.edu.tw>
2019-02-10 16:45:17 +0800

    Initial Commit

---
+++ storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyboard
+//
+//  Created by 吳佳臻 on 2019/2/10.
+//  Copyright © 2019 ChiaChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
